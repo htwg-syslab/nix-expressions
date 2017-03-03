b9472c0fbac63e86e147db547b5242b10c26b3ed
Stefan Junker <mail@stefanjunker.de>
2017-03-03 21:40:26 +0100

    pkgs/nixshwrap: interpete with sh and exec nix-shell

--- pkgs/nixshwrap/default.nix
+++ pkgs/nixshwrap/default.nix
@@ -8,11 +8,19 @@
 mkDerivation rec {
   name = "nixshwrap";
   version = "0.1.0";
+# TODO: examine why this does not work with htts derivations in nix-shell
+#  src = writeScript "nixSHwrap" ''
+#    #! ${pkgs.nix}/bin/nix-shell
+#    #! nix-shell --argstr shDrv ${shDrv}
+#    #! nix-shell -A ${shDrvAttr} 
+#    #! nix-shell ${shDrv}
   src = writeScript "nixSHwrap" ''
-    #! ${pkgs.nix}/bin/nix-shell
-    #! nix-shell --argstr shDrv ${shDrv}
-    #! nix-shell -A ${shDrvAttr} 
-    #! nix-shell ${shDrv}
+    #! ${pkgs.bash}/bin/sh
+    exec \
+      ${pkgs.nix}/bin/nix-shell \
+        --argstr shDrv ${shDrv} \
+        -A ${shDrvAttr} \
+        ${shDrv}
   '';
   unpackCmd = ''
     mkdir src
