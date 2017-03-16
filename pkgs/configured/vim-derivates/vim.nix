f4cd896ec2fc879319f2eea7b752354768836732
Stefan Junker <mail@stefanjunker.de>
2017-03-16 20:53:40 +0100

    add configured vim

---
+++ pkgs/configured/vim-derivates/vim.nix
@@ -0,0 +1,30 @@
+{ pkgs
+, name
+, additionalRC ? ""
+, additionalPlugins ? []
+, ... } @ args :
+
+pkgs.vim_configurable.customize {
+  inherit name;
+  # add custom .vimrc lines like this:
+  vimrcConfig.customRC = (import ./commonrc.nix {}) + ''
+  '' + additionalRC;
+
+  vimrcConfig.vam.knownPlugins = pkgs.vimPlugins; # optional
+  vimrcConfig.vam.pluginDictionaries = [{ 
+    # full ducomentation at github.com/MarcWeber/vim-addon-manager
+    names = [
+      "vim-addon-vim2nix"
+      "vim-airline"
+      "vim-addon-nix"
+      "ctrlp"
+      "syntastic"
+      "vim-css-color"
+      "rainbow_parentheses"
+      "vim-colorschemes"
+      "vim-colorstepper"
+      "vim-signify"
+      "youcompleteme"
+    ] ++ additionalPlugins;
+  }];
+}
