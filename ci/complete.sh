9451b1bbe5cd85bc13aadf256862e00c023b712b
Stefan Junker <mail@stefanjunker.de>
2017-04-28 14:21:52 +0200

    ci: print msg minutely to prevent timeout

--- ci/complete.sh
+++ ci/complete.sh
@@ -4,5 +4,15 @@ set -e
 scriptdir=$(cd $(dirname $0); pwd)
 source $scriptdir/source.sh
 
+(
+    echo Running dummy background job to prevent CI from killing this script.
+    while true; do
+        sleep 60
+        echo $0 is still working...
+    done
+) &
+# Don't leave any children behind
+trap "for j in `jobs -p`; do kill -9 \$j; done" EXIT
+
 $scriptdir/install.sh
 $scriptdir/test.sh
